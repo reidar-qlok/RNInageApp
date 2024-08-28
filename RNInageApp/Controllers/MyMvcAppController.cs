8de4d5bae0033b763570083f404701b531e7a62f
Reidar Nilsen <4reidar@gmail.com>
2024-08-28 14:11:19 +0200

    Add project files.

---
+++ RNInageApp/Controllers/MyMvcAppController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RNInageApp.Controllers
+{
+    public class MyMvcAppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult UploadImage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> ClassifyImage(IFormFile imageFile)
+        {
+            if (imageFile == null || imageFile.Length == 0)
+            {
+                ViewBag.Message = "Du glömde nog att lägga till en bild";
+                return View("UploadImage");
+            }
+            // konvertera bilden till en bytearray
+            byte[] imageBytes;
+            using (var memoryStream = new MemoryStream())
+            {
+                await imageFile.CopyToAsync(memoryStream);
+                imageBytes = memoryStream.ToArray();
+            }
+            // Skapa en modelinput och sätta en bildkälla
+            var modelInput = new BildKlassModel.ModelInput
+            {
+                ImageSource = imageBytes
+            };
+            //Använd denna modellen för att göra en forutsägelse
+            var prediction = BildKlassModel.Predict(modelInput);
+            //Hämta och visa resultatet
+            ViewBag.PredictionLabel = prediction.PredictedLabel;
+            ViewBag.Scores = BildKlassModel.GetSortedScoresWithLabels(prediction);
+
+            return View("UploadImage");
+        }
+    }
+}
---
+++ RNInageApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RNInageApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
